c29bd9c74ee8f86e91d987ab7d4cd7e1e45770b7
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-10-14 18:13:49 -0600

    añadir el formulario reactivo

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  styleUrls: ['./product-page.component.css'],
 })
 export class ProductPageComponent {
+  //similar a poner en el constructor private fb: FormBuilder
+  private fb = inject(FormBuilder);
+  public myForm = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.minLength(6), Validators.email],
+    ],
+  });
+  public color: string = '';
 
+  changeColor() {
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    this.color = color;
+  }
 }
